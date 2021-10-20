8985ef65be78ff71b526c4e9f87cf37065463f14
Shahabgeravesh <Shahabgeravesh@gmail.com>
2021-10-19 19:39:46 -0700

    required path

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const path = require('path');
 const session = require("express-session");
 const passport = require("./config/passport.js");
 const db = require("./models");
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 require("dotenv").config();
